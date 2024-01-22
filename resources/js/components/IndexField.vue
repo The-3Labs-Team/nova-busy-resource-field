6651110c12da460f196335e83345b4917321b0a2
Stefano Novelli <murdercode@gmail.com>
2024-01-22 10:41:30 +0100

    wip

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,8 +1,11 @@
 <template>
 
-    <div v-if="fieldData" class="absolute bg-white p-4 rounded-lg shadow-lg hidden" :class=" 'details-box-'+fieldData.pivot.busiable_id" style="transform: translate(40px, -25px); z-index: 5;">
+    <div v-if="fieldData" :class=" 'details-box-'+fieldData.pivot.busiable_id"
+         class="absolute bg-white p-4 rounded-lg shadow-lg hidden"
+         style="transform: translate(40px, -25px); z-index: 5;">
 
-        <div class="absolute" style="top: 50%; right: 100%; transform: translateY(-50%); border-top: 10px solid transparent; border-right: 20px solid white; border-bottom: 10px solid transparent;"></div>
+        <div class="absolute"
+             style="top: 50%; right: 100%; transform: translateY(-50%); border-top: 10px solid transparent; border-right: 20px solid white; border-bottom: 10px solid transparent;"></div>
 
         <p class="relative z-2">
             <span class="text-base">Occupied by <span class="font-bold">{{ fieldData.name }}</span></span>
@@ -11,9 +14,20 @@
         </p>
     </div>
 
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" @mouseover="showDetails" @mouseout="showDetails">
-        <path v-if="fieldValue === 'Busy'" class="text-red-500" stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-        <path v-else stroke-linecap="round" style="color: #b5b5b5;" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+    <svg v-if="fieldValue === 'Busy'" class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5"
+         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @mouseout="showDetails" @mouseover="showDetails">
+        <path class="text-red-500"
+              d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+              stroke-linecap="round"
+              stroke-linejoin="round"/>
+    </svg>
+
+    <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+         xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+            stroke-linecap="round" stroke-linejoin="round"
+            style="color: #b5b5b5;"/>
     </svg>
 
 </template>
@@ -40,8 +54,8 @@ export default {
                         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
                     },
                     body: JSON.stringify({
-                      'model-id': this.resource.id.value,
-                      'model-name': this.$page.props.resourceName,
+                        'model-id': this.resource.id.value,
+                        'model-name': this.$page.props.resourceName,
                     }),
                 });
 
